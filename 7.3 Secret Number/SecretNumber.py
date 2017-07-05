7a1e0ed03916a4ccbcbfe3dd86a6407eb5f9a71c
Julia Rose <purpur_rose@hotmail.com>
2017-07-05 12:48:08 +0200

    added an error message for invalid characters

--- 7.3 Secret Number/SecretNumber.py
+++ 7.3 Secret Number/SecretNumber.py
@@ -1,7 +1,11 @@
 secret = 42
-guess = int(raw_input ("Guess the secret number: "))
+guess = raw_input ("Guess the secret number: ")
 
-if guess == secret:
-    print "Congratulations! You have won 1 Million Dollars!"
-else:
-    print "Sorry, try again!"
+try:
+    if int(guess) == secret:
+        print "Congratulations! You have won 1 Million Dollars!"
+    else:
+        print "Sorry, try again!"
+
+except ValueError as e:
+     print "Please only guess numbers or you'll never win!"

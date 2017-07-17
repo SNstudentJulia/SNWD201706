1720a72a7741a7e7cb9d03e51476d15229091f2c
Julia Rose <purpur_rose@hotmail.com>
2017-07-17 15:18:13 +0200

    changed the operation of FizzBuzz

--- 8.2 FizzBuzz/FizzBuzz.py
+++ 8.2 FizzBuzz/FizzBuzz.py
@@ -1,25 +1,27 @@
 answer = raw_input("Select a number between 1 and 100: ")
+start = 0
 
 try:
     if int(answer) >= 101:
         print "Please only enter valid numbers between 1 and 100."
-
+    elif int(answer) == 0:
+        print "Please only enter valid numbers between 1 and 100."
     else:
-        number = int(answer) - 1
+        number = int(answer)
 
         while True:
-            number += 1
+            start += 1
 
-            if (number % 3) + (number % 5) == 0:
-                print "fizzbuzz"
-            elif number % 3 == 0:
-                print "fizz"
-            elif number % 5 == 0:
-                print "buzz"
-            elif number >= 101:
+            if start > number:
                 break
+            elif (start % 3) + (start % 5) == 0:
+                print "fizzbuzz"
+            elif start % 3 == 0:
+                print "fizz"
+            elif start % 5 == 0:
+                print "buzz"
             else:
-                print number
+                print start
 
 except ValueError:
      print "Please only enter valid numbers."

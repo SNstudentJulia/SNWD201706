63e353636ccb009e575bc62efebd86743a715425
Julia Rose <purpur_rose@hotmail.com>
2017-09-03 18:44:16 +0200

    edited NEWSecretNumber with ValueError

--- 10.2 NEW Secret Number/NEWSecretNumber.py
+++ 10.2 NEW Secret Number/NEWSecretNumber.py
@@ -1,20 +1,22 @@
 import random
 
 def main():
-    secret = random.randint(1, 5)
-    while True:
-        guess = int(raw_input("Guess the secret number 1 to 5: "))
-        if guess == secret:
-            print "Congratulations! You have won 1 Million Dollars!"
-            break
-
-        elif guess != secret:
-            again = raw_input("Try again? (yes/no) ")
-            if again.lower() in ("no", "n"):
-                print "Game Over"
+    try:
+        secret = random.randint(1, 5)
+        while True:
+            guess = int(raw_input("Guess the secret number 1 to 5: "))
+            if guess == secret:
+                print "Congratulations! You have won 1 Million Dollars!"
                 break
-    print "End"
 
+            elif guess != secret:
+                again = raw_input("Try again? (yes/no) ")
+                if again.lower() in ("no", "n"):
+                    print "Game Over"
+                    break
+        print "End"
+    except ValueError as e:
+        print "Please only guess numbers or you'll never win!"
 
 if __name__ == "__main__":
             main()
